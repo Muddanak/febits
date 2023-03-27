dbde8ba845837c7d5d02ec17e60ccb01907a4370
Steve <Steve@Steve.steve>
2023-03-26 23:05:39 -0500

    Added some clarification in the vectors sorting function
    Increased version 0.1.0 -> 0.1.1

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,5 +1,6 @@
 
 /// Returns a sorted [Vec] of [f32] values from smallest -> largest
+/// This is an application of the example code provided in the Rust std [slice] library, [sort_by]
 ///
 /// # Example
 ///
@@ -13,11 +14,12 @@
 pub fn sort_vec_floats_f32(v: &Vec<f32>) -> Vec<f32> {
     let mut cvec: Vec<f32> = v.clone();
 
-    cvec.sort_by(|x:&f32, y:&f32| {
-        let Some(res) = x.partial_cmp(y) else { panic!("Error in value to sort, unexpected value!") };
+    cvec.sort_by(|first:&f32, second:&f32| {
+        let Some(res) = first.partial_cmp(second) else { panic!("Error in value to sort, unexpected value!") };
         res
     }
     );
+
 
     cvec.to_owned()
 }
